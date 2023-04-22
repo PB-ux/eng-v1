9449a34d6b3e6621b4caba1a8606194bd81d6b45
Pavel Bratkov <bratkov.pasha21@gmail.com>
2023-04-22 18:15:02 +0300

    refactoring upload avatar

--- src/components/platform/Account/InfoAccount.jsx
+++ src/components/platform/Account/InfoAccount.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import cn from 'classnames';
 
 import  { uploadPhoto } from '../../../store/asyncActions/users';
 
@@ -9,18 +8,17 @@ function InfoAccount(props) {
     const user = useSelector((state) => state.user.user);
 
     const [file, setFile] = useState();
-    const [isVisible, setVisible] = useState(false);
+
+    useEffect(() => {
+        if (file) handleSubmit();
+    }, [file]);
+
 
     function handleChange(event) {
         setFile(event.target.files[0]);
-        setVisible(true);
     }
 
-    function handleSubmit(event) {
-        event.preventDefault()
-
-        setVisible(false);
-
+    function handleSubmit() {
         const formData = new FormData();
         formData.append('file', file);
 
@@ -32,8 +30,8 @@ function InfoAccount(props) {
             <img className="account__info-avatar" src={`http://localhost:5000/${user.photo}`}></img>
             <form className="account__info-form">
                 <input type="file" id="file" className="account__info-file" onChange={handleChange}/>
-                <label className={cn('account__info-label', { 'account__info-label_visible': isVisible })} htmlFor="file">
-                    { isVisible ? <button className="account__info-overlay_btn" type="submit" onClick={handleSubmit}>Подтвердить</button> : <span className="account__info-overlay">Загрузить фото</span> }
+                <label className="account__info-label" htmlFor="file">
+                    <span>Загрузить фото</span>
                 </label>
             </form>
         </div>
