7fc17e9f9e700901cc4bdfd18c13733370f8b5e5
sietebonduy <den768653@gmail.com>
2023-04-24 22:18:31 -0700

    disabled locked links in book card

--- src/components/UI/LevelBook.jsx
+++ src/components/UI/LevelBook.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import cn from 'classnames';
 
 function LevelBook(props) {
     return <div className="page-book__level">
--- src/components/platform/Library/CardBook.jsx
+++ src/components/platform/Library/CardBook.jsx
@@ -1,14 +1,17 @@
 import React, {useState} from 'react';
 import cn from 'classnames';
 
+import jwt_decode from 'jwt-decode';
+
 import {LEVEL_LANGUAGE} from '../../constansts/LibraryConstants';
 
 import LevelCard from '../../UI/LevelCard.jsx';
 
 import { AiFillHeart } from 'react-icons/Ai';
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
 
 function Card({ id, level, preview, title, author, tooltip }) {
+
     const levelClassName = cn('library__card-content', {
         'library__card_a1': level === LEVEL_LANGUAGE.A1,
         'library__card_a2': level === LEVEL_LANGUAGE.A2,
@@ -18,13 +21,17 @@ function Card({ id, level, preview, title, author, tooltip }) {
     });
     const [isActive, setActive] = useState(false);
 
+    const token = localStorage.getItem('token');
+    const dataUser = jwt_decode(token);
+    const linkStyle = dataUser.level === level ? 'library__card-title_enable' : 'library__card-title_disable';
+
     return <div className="library__card">
         <div className={levelClassName}>
             <div className={cn("library__card-icon", {"library__card-icon_active": isActive})} onClick={() => setActive(!isActive) }><AiFillHeart /></div>
             <img className="library__card-preview" src={preview} alt={title} />
             <LevelCard level={level} />
         </div>
-        <Link className="library__card-title" to={`book/${id}`}>{title}</Link>
+        <Link className={linkStyle} to={`book/${id}`}>{title}</Link>
         <div className="library__card-author">{author}</div>
     </div>
 }
